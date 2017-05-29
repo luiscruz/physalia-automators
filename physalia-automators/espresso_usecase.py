4ff3235470be0f99a614feea2c2d44c2cd62dbd3
Luis Cruz <luismirandacruz@gmail.com>
2017-05-29 18:42:02 -0300

    added dictionaire with all usecases for Espresso

--- physalia-automators/espresso_usecase.py
+++ physalia-automators/espresso_usecase.py
@@ -131,7 +131,7 @@ tap_use_case = EspressoUseCase(
     time_boundaries.TAP
 )
 
-print tap_use_case.run().duration
+# print tap_use_case.run().duration
 
 # -------------------------------------------------------------------------- #
 
@@ -147,7 +147,7 @@ long_tap_use_case = EspressoUseCase(
     time_boundaries.LONG_TAP
 )
 
-print long_tap_use_case.run().duration
+# print long_tap_use_case.run().duration
 
 # -------------------------------------------------------------------------- #
 
@@ -163,7 +163,7 @@ swipe_use_case = EspressoUseCase(
     time_boundaries.SWIPE
 )
 
-print swipe_use_case.run().duration
+# print swipe_use_case.run().duration
 # -------------------------------------------------------------------------- #
 
 back_button_use_case = EspressoUseCase(
@@ -178,7 +178,7 @@ back_button_use_case = EspressoUseCase(
     time_boundaries.BACK_BUTTON
 )
 
-print back_button_use_case.run().duration
+# print back_button_use_case.run().duration
 
 # -------------------------------------------------------------------------- #
 
@@ -195,3 +195,17 @@ input_text_use_case = EspressoUseCase(
 )
 
 # print input_text_use_case.run().duration
+
+use_cases = {
+    "find_by_id": find_by_id_use_case,
+    "find_by_description": find_by_description_use_case,
+    "find_by_content": find_by_content_use_case,
+    "tap": tap_use_case,
+    "long_tap": long_tap_use_case,
+    "multi_finger_tap": None,
+    "dragndrop": None,
+    "swipe": swipe_use_case,
+    "pinch_and_spread": None,
+    "back_button": back_button_use_case,
+    "input_text": input_text_use_case,
+}
