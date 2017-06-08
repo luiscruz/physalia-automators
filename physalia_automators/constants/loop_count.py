770f8939c6a7d092524777441d0fd3374295dda4
Luis Cruz <luismirandacruz@gmail.com>
2017-06-08 17:43:07 +0100

    added constants

---
+++ physalia_automators/constants/loop_count.py
@@ -0,0 +1,10 @@
+"""Number of iterations for the loops in the experiments."""
+
+FIND_BY_ID = 40
+FIND_BY_DESCRIPTION = 40
+FIND_BY_CONTENT = 40
+TAP = 10
+DRAGNDROP = 10
+SWIPE = 40
+PINCH_AND_SPREAD = 40
+BACK_BUTTON = 200
